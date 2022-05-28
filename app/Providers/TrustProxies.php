573bd5fd114d2d695f656b2c370d617df38d321f
Ihsan Devs <ichsanhafizdalfajry@gmail.com>
2022-05-28 19:29:41 +0700

    git add .

---
+++ app/Providers/TrustProxies.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\ServiceProvider;
+
+class TrustProxies extends ServiceProvider
+{
+    protected $headers =
+    Request::HEADER_X_FORWARDED_FOR |
+    Request::HEADER_X_FORWARDED_HOST |
+    Request::HEADER_X_FORWARDED_PORT |
+    Request::HEADER_X_FORWARDED_PROTO |
+    Request::HEADER_X_FORWARDED_AWS_ELB;
+
+
+    protected $proxies = "*";
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}

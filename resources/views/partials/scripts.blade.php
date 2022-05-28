5e37f29cea08302cd962e6393d4c5eeaa851fa86
Ihsan Devs <ichsanhafizdalfajry@gmail.com>
2022-05-28 18:39:07 +0700

    Removed code for disable right click

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -23,33 +23,4 @@ data-turbolinks-eval="false"></script>
         const lightbox = new Lightbox(el, options);
         lightbox.show();
     }));
-
-
-    /**
-     * Function for disable right click
-     */
-    var message = "Function Disabled!";
-
-    function clickIE4() {
-        if (event.button == 2) {
-            alert(message);
-            return false;
-        }
-    }
-
-    function clickNS4(e) {
-        if (document.layers || document.getElementById && !document.all) {
-            if (e.which == 2 || e.which == 3) {
-                alert(message);
-                return false;
-            }
-        }
-    }
-    if (document.layers) {
-        document.captureEvents(Event.MOUSEDOWN);
-        document.onmousedown = clickNS4;
-    } else if (document.all && !document.getElementById) {
-        document.onmousedown = clickIE4;
-    }
-    document.oncontextmenu = new Function("return false");
 </script>

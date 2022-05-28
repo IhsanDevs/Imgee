6a0ef4ac0440de7f7e515553eda8bfeb09f908f1
Ihsan Devs <ichsanhafizdalfajry@gmail.com>
2022-05-28 19:35:37 +0700

    hmmm

--- app/Providers/TrustProxies.php
+++ app/Providers/TrustProxies.php
@@ -7,14 +7,6 @@ use Illuminate\Support\ServiceProvider;
 
 class TrustProxies extends ServiceProvider
 {
-    protected $headers =
-    Request::HEADER_X_FORWARDED_FOR |
-    Request::HEADER_X_FORWARDED_HOST |
-    Request::HEADER_X_FORWARDED_PORT |
-    Request::HEADER_X_FORWARDED_PROTO |
-    Request::HEADER_X_FORWARDED_AWS_ELB;
-
-
     protected $proxies = "*";
     /**
      * Register services.
